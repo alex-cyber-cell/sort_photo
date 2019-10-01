33e10137abee16ba459d8b2e82f23569c07afea1
Aqualex <alex.risnoveanu@aquaq.co.uk>
2019-10-02 00:36:42 +0100

    ADDED new stuff

---
+++ sort_photo.py
@@ -0,0 +1,55 @@
+###############################################################################
+##
+##  Script Name: sort_photo.py
+##       Author: Unknown
+##         Year: 2019
+##        Month: October
+##  Description: Script which sorts photos by date
+##
+###############################################################################
+
+###############################################################################
+##  ENVIRONMENT VARIABLES
+###############################################################################
+
+
+
+###############################################################################
+##  IMPORTS
+###############################################################################
+
+
+
+###############################################################################
+##  FUNCTION DEFINITION
+###############################################################################
+
+def getAvailableSpace():
+    #function to get the available space on target device 
+    print('getAvailableSpace: Executed ...');
+    
+def getNecessarySpace():
+    #function to get the necessary space on target device 
+    print('getNecessarySpace: Executed ...');
+    
+def compareSpace():
+    #function to compare the necessary and avaialble space
+    print('compareSpace: Executed ...');
+    
+def getPhotoDate():
+    #function to get the date when the photo was taken 
+    print('getPhotoDate: Executed ...');
+    
+def createDateFolder():
+    #function to check if folder for given date exists in target location and 
+    #creates it if not 
+    print('createDateFolder: Executed ...');
+
+def main ():
+    print('MAIN: Executed ...');
+
+###############################################################################
+##  MAIN
+###############################################################################
+
+main();    
