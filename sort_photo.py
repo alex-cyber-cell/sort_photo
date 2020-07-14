747231c402c2652494845b2cbd8d3ad229a148bc
alex-cyber-cell <rdanutalexandru@gmail.com>
2020-07-14 02:27:27 +0100

    SWITCHED to argparse for command line arguments reading

--- sort_photo.py
+++ sort_photo.py
@@ -1,47 +1,39 @@
-###############################################################################
+########################################################################################################################
 ##
 ##  Script Name: sort_photo.py
 ##       Author: Unknown
-##         Year: 2019
+##         Year: 2020
 ##        Month: October
 ##  Description: Script which sorts photos by date
 ##
-###############################################################################
+########################################################################################################################
 
-###############################################################################
+########################################################################################################################
+##     USEFUL LINKS
+########################################################################################################################
+#Logging - https://www.toptal.com/python/in-depth-python-logging
+
+########################################################################################################################
 ##  ENVIRONMENT VARIABLES
-###############################################################################
+########################################################################################################################
 
 
-
-###############################################################################
+########################################################################################################################
 ##  IMPORTS
-###############################################################################
+########################################################################################################################
 
-import sys, getopt
+import sys, getopt, logging, argparse, os
 
-###############################################################################
+########################################################################################################################
 ##  FUNCTION DEFINITION
-###############################################################################
+########################################################################################################################
+class Photo:
+    def __init__():
+        print("photo class has been initialsed")
 
-
-class log:
-    #class to manage log file and printing to screen
-    def __init__(self, logFilePath):
-        self.logFilePath=logFilePath
-
-    def logTime(self):
-        return datetime.datetime.now().strftime("%Y.%m.%dD%H.%M.%S.%f")
-
-    def lvl(self, level):
-        if (level == 0):
-            prefix = "[*INFO]|"
-        elif (level == 1):
-            prefix = "[ERROR]|"
-        else:
-            prefix = "[DEBUG]|"
-        return prefix + getTimestamp() + "| " + message
-
+########################################################################################################################
+##  FUNCTION DEFINITION
+########################################################################################################################
 def getAvailableSpace():
     #function to get the available space on target device 
     print('getAvailableSpace: Executed ...')
@@ -54,42 +46,41 @@ def compareSpace():
     #function to compare the necessary and avaialble space
     print('compareSpace: Executed ...')
     
-def getPhotoDate():
-    #function to get the date when the photo was taken 
-    print('getPhotoDate: Executed ...')
-    
 def createDateFolder():
     #function to check if folder for given date exists in target location and 
     #creates it if not 
     print('createDateFolder: Executed ...')
 
 def getCmdLineArguments():
-    # function to create a dictionary of arguments passed from the cmdline
-    dictVal = {}  # Creating an empty dictionary
-    argv = sys.argv[1:]
-    try:
-        opts, args = getopt.getopt(argv, "hd:f:t:l:",["SORTPHOTO_FROM=", "SORTPHOTO_TO=", "SORTPHOTO_LOG="])
-    except getopt.GetoptError:
-        print('getCmdLineArguments: Failed to get command line arguments ...')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print(
-                "\nHelp Message:\nsort_photo.py --SORTPHOTO_FROM [path] --SORTPHOTO_TO [path] --SORTPHOTO_LOG [path]\n")
-            sys.exit()
-        elif opt in ("-f", "--SORTPHOTO_FROM"):
-            dictVal['SORTPHOTO_FROM'] = arg
-        elif opt in ("-t", "--SORTPHOTO_TO"):
-            dictVal['SORTPHOTO_TO'] = arg
-        elif opt in ("-l", "--SORTPHOTO_LOG"):
-            dictVal['SORTPHOTO_LOG'] = arg
-    return dictVal
+    #function to get command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-sourceDir',
+                        '--sourceDirectory',
+                        required=True,
+                        help='The directory where all the photos are')
+    parser.add_argument('-destDir',
+                        '--destDirectory',
+                        required=True,
+                        help='Directory where files should be sorted')
+    parser.add_argument('-logDir',
+                        '--logDirectory',
+                        help="""Directory where the log file should be dumped.
+                                If empty this will be set to home dir""")
+    args = parser.parse_args()
+    if(args.logDirectory==None):
+        print('Log Directory has not been provided. Setting to script directory')
+        args.logDirectory = os.path.dirname(sys.argv[0]) + '/'
+        #Adding the log directory
+        args.logDirectory = args.logDirectory + 'log/'
+        os.makedirs(args.logDirectory, exist_ok=True)
+        print('Log folder is now set to ' + args.logDirectory)
+    return args
 
 def main ():
     dictVal = getCmdLineArguments()
 
-###############################################################################
+########################################################################################################################
 ##  MAIN
-###############################################################################
+########################################################################################################################
 if __name__ == "__main__":
     main()
