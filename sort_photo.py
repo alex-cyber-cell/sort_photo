d799eee2df8a8d54937bbb035bbd2f4fb62989de
alex-cyber-cell <rdanutalexandru@gmail.com>
2020-07-16 01:45:51 +0100

    ADDED new stuff

--- sort_photo.py
+++ sort_photo.py
@@ -13,33 +13,57 @@
 ########################################################################################################################
 #Logging - https://www.toptal.com/python/in-depth-python-logging
 
-########################################################################################################################
-##  ENVIRONMENT VARIABLES
-########################################################################################################################
-
 
 ########################################################################################################################
-##  IMPORTS
+##     GLOBAL VARIABLES
 ########################################################################################################################
 
-import sys, getopt, logging, argparse, os, datetime
+########################################################################################################################
+##     ENVIRONMENT VARIABLES
+########################################################################################################################
+fileFrmt = {'jpg'  : 'jpg',
+            'JPEG' : 'jpg',
+            'JPG'  : 'jpg',
+            'mp4'  : 'mp4'}
+########################################################################################################################
+##     IMPORTS
+########################################################################################################################
+
+import sys, getopt, logging, argparse, os, datetime, shutil
 
 from PIL import Image
 
 ########################################################################################################################
-##  FUNCTION DEFINITION
+##     FUNCTION DEFINITION
 ########################################################################################################################
 class Photo:
-    def __init__(self, path):
-        print('classPhoto: (init) Executed ...')
-        self.photoPath = path
+    def __init__(self, path, dest):
+        self.photoSource = path
+        self.coreDest = dest
         self.cDate = self.getCreationDate()
+        self.photoDest = self.buildDestPath()
+
+    def buildDestPath(self):
+        #create a destination path
+        return self.coreDest + self.cDate + '/'
 
     def getCreationDate(self):
-        return Image.open(self.photoPath)._getexif()[36867].split(' ')[0].replace(':','')
+        #get the date of creation
+        return Image.open(self.photoSource)._getexif()[36867].split(' ')[0].replace(':','')
+
+    def createPath(self):
+        #create the necessary directory where the file will be copied
+        print("class[Photo]|Method[copyFile]: Creating directory " + self.photoDest)
+        os.makedirs(self.photoDest, exist_ok=True)
+
+    def copyFile(self):
+        #method to copy the file across
+        print("class[Photo]|Method[copyFile]: Copying file " + (self.photoSource.split('/')[-1]) + " to " + self.photoDest)
+        self.createPath()
+        shutil.copy(self.photoSource, self.photoDest)
 
 ########################################################################################################################
-##  FUNCTION DEFINITION
+##     FUNCTION DEFINITION
 ########################################################################################################################
 def getAvailableSpace():
     #function to get the available space on target device 
@@ -71,13 +95,25 @@ def getFiles(src):
 
 def copyFilesAcross(LOF,dest):
     print('copyFilesAcross: Executed ...')
-    print('copyFilesAcross: Creating list of objects for photos')
-    objs = []
+    #print('copyFilesAcross: Creating list of objects for photos')
+    #objs = []
     for fl in LOF:
-        if(fl.split('.')[-1] == 'jpg'):
-            objs.append(Photo(fl))
+        #get the file format
+        fFrmt = fl.split('.')[-1]
+        #if mapping exist map, otherwise inform
+        if(fFrmt in fileFrmt.keys()):
+            fFrmt = fileFrmt[fFrmt]
         else:
-            print('copyFilesAcross: Not implemented for extension: ' + fl.split('.')[-1])
+            print("Mapping does not exist for " + fFrmt)
+        #execute specific class based on type
+        if( fFrmt == 'jpg'):
+            #objs.append(Photo(fl, dest))
+            photo = Photo(fl, dest)
+            photo.copyFile()
+        elif (fFrmt == 'mp4'):
+            print("Not Yet Implemented for " + fFrmt)
+        else:
+            print("Not yet Implemented for " + fFrmt)
 
 
 def getCmdLineArguments():
